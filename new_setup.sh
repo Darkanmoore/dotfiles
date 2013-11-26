7c3f8d3556356401fa7e609632ec7fc79de40fa1
smlb <simone.lom02@gmail.com>
2013-11-26 09:31:58 +0100

    Create new_setup.sh

---
+++ new_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Very simple script for fresh installation -- Nothing else ŁØŁ
+
+cd $HOME
+git clone https://github.com/smlb/dotfiles.git
+cd dotfiles
+
+mv * $HOME
+mv ~/ncmpcpp ~/.ncmpcpp
+rm README.md 
+
+echo "Done!"
